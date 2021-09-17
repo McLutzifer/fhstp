0902adcf504f71fb66487b262d2eea6691ca601d
McLutzifer <lukasschumi@gmx.at>
2021-09-17 20:22:13 +0200

    update

---
+++ CPP/WTS/script.js
@@ -0,0 +1,10 @@
+function onButtonClick() {
+    document.getElementById('clickme').innerHTML = "Hidden &#x1F984";
+    console.log('Don\'t look at the console, look at the unicorn!');
+    alert("congratulations! You found the unicorn!");
+}
+
+function colorChange() {
+    document.getElementById('headingh2').style.color = 'red';
+    document.getElementById('headingh2').style.backgroundColor = 'yellow';
+}
