7d2b964b8babc627d47faf76312ac6b88e42c7f1
McLutzifer <lukasschumi@gmx.at>
2021-09-17 14:52:44 +0200

    update

--- CPP/WTS/script.js
+++ CPP/WTS/script.js
@@ -1,5 +1,9 @@
 function onButtonClick() {
     document.getElementById('clickme').innerHTML = "Hidden &#x1F984";
-    console.log('Secret message');
-    alert("Welcome back!");
+    console.log('Don\'t look at the console, look at the unicorn!');
+    alert("congratulations! You found the unicorn!");
+}
+
+function colorChange() {
+    document.getElementById('headingh2').style.color = 'red';
 }
