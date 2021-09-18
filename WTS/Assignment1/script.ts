2f39ad06944a9b90dce0381424f68255dc036f91
McLutzifer <lukasschumi@gmx.at>
2021-09-18 11:22:11 +0200

    update

--- WTS/Assignment1/script.ts
+++ WTS/Assignment1/script.ts
@@ -1,4 +1,5 @@
-function buttonClick() {
+
+const buttonClick = () => {
     document.getElementById('mainHeading').innerHTML = "Nice, you broke it!";
     document.getElementById('button').style.display = "none";
     document.getElementById('menu').style.display = "none";
