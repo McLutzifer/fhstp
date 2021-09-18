fffd027d7dc535be7893a986b40a44b2a26afac0
McLutzifer <lukasschumi@gmx.at>
2021-09-18 10:56:03 +0200

    update

---
+++ WTS/Assignment1/script.js
@@ -0,0 +1,5 @@
+function buttonClick() {
+    document.getElementById('mainHeading').innerHTML = "Nice, you broke it!";
+    document.getElementById('button').style.display = "none";
+    document.getElementById('menu').style.display = "none";
+}
--- WTS/lecture_2/script.js
+++ WTS/lecture_2/script.js
@@ -1,6 +1,18 @@
 // HEADING
-document.getElementById("toptitle").innerHTML = "Smurf";
+document.getElementById('toptitle').innerHTML = "Smurf";
+document.getElementById('toptitle').style.fontSize = "12px";
+document.getElementById('toptitle').innerHTML = 3+4;
 
 // LOGO
 document.getElementById('fhlogo').src = "super-poop.png";
 document.getElementById('fhlogo').style.width = "200px";
+//document.getElementById('fhlogo').style.display = "center"
+
+
+alert(13+7);
+
+
+//variable
+var variable1;
+let variable2;
+// const variable3;
