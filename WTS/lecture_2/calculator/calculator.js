02abfc3597f1aa4fc654a4454d339b2896c03d96
McLutzifer <lukasschumi@gmx.at>
2021-09-18 12:11:10 +0200

    calculator

---
+++ WTS/lecture_2/calculator/calculator.js
@@ -0,0 +1,14 @@
+  function getInput(num1, operator, num2) {
+      if (operator == "+") {
+          alert(num1 + num2);
+      };
+  }; 
+
+
+
+  function getInputValue(){
+    var inputVal = document.getElementById("myInput").value;
+    
+    // Displaying the value
+    alert(inputVal);
+}
