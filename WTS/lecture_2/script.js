566c61fafea79246cddc1c3397b160e84e882b0a
McLutzifer <lukasschumi@gmx.at>
2021-09-18 10:17:07 +0200

    update

---
+++ WTS/lecture_2/script.js
@@ -0,0 +1,6 @@
+// HEADING
+document.getElementById("toptitle").innerHTML = "Smurf";
+
+// LOGO
+document.getElementById('fhlogo').src = "super-poop.png";
+document.getElementById('fhlogo').style.width = "200px";
