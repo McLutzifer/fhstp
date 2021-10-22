9c0c1b2957e0fda1881114d326621657877a4ff4
McLutzifer <lukasschumi@gmx.at>
2021-10-22 15:38:11 +0200

    json

--- WTS/JS/chess/chess/script.js
+++ WTS/JS/chess/chess/script.js
@@ -1,38 +1,81 @@
+var chessBoard =  {
+    "fieldCount": 64,
+"widthAndHeight": 60,
+"fields": [],
+
+"drawChessboard": function() {
+        for (let j = 0; j < 8; j++) {
+        for (let i = 0; i < 8; i++) {
+        let field = document.createElement("div");
+        field.style.position = "absolute";
+        field.style.width = widthAndHeight + "px";
+        field.style.height = widthAndHeight + "px";
+        field.style.left = i * widthAndHeight + "px";
+        field.style.top = j * widthAndHeight + "px";
+        let color = startColor;
+        if((i+j) % 2 === 0) {
+            if (startColor == "#000")
+                color = "#fff";
+            else
+                color = "#000";
+        }
+        field.style.backgroundColor = color;
+        field.id = i + j * 8;
+        fields.push(field);
+        document.getElementById("chess").appendChild(field);
+    }
+    }
+},
+"invertColor": function () {
+    for (var i = 0; i < fields.length; i++) {
+    var color = fields[i].style.backgroundColor;
+    if(color == "rgb(255, 255, 255)") {
+        fields[i].style.backgroundColor = "#000";
+    }
+    else {
+        fields[i].style.backgroundColor = "#fff";
+    }
+    }   
+}
+
+}
+
+
 let fields = [];
 function drawChessboard (startColor) {
-    let widthAndHeight = 60;
-    for (let j = 0; j < 8; j++) {
-        for (let i = 0; i < 8; i++) {
-            let field = document.createElement("div");
-            field.style.position = "absolute";
-            field.style.width = widthAndHeight + "px";
-            field.style.height = widthAndHeight + "px";
-            field.style.left = i * widthAndHeight + "px";
-            field.style.top = j * widthAndHeight + "px";
-            let color = startColor;
-            if((i+j) % 2 === 0) {
-                if (startColor == "#000")
-                    color = "#fff";
-                else
-                    color = "#000";
-            }
-            field.style.backgroundColor = color;
-            field.id = i + j * 8;
-            fields.push(field);
-            document.getElementById("chess").appendChild(field);
+let widthAndHeight = 60;
+for (let j = 0; j < 8; j++) {
+    for (let i = 0; i < 8; i++) {
+        let field = document.createElement("div");
+        field.style.position = "absolute";
+        field.style.width = widthAndHeight + "px";
+        field.style.height = widthAndHeight + "px";
+        field.style.left = i * widthAndHeight + "px";
+        field.style.top = j * widthAndHeight + "px";
+        let color = startColor;
+        if((i+j) % 2 === 0) {
+            if (startColor == "#000")
+                color = "#fff";
+            else
+                color = "#000";
         }
- 
+        field.style.backgroundColor = color;
+        field.id = i + j * 8;
+        fields.push(field);
+        document.getElementById("chess").appendChild(field);
     }
+
+}
 }
 
 function invertColor() {
-    for (var i = 0; i < fields.length; i++) {
-        var color = fields[i].style.backgroundColor;
-        if(color == "rgb(255, 255, 255)") {
-            fields[i].style.backgroundColor = "#000";
-        }
-        else {
-            fields[i].style.backgroundColor = "#fff";
-        }
-    }   
+for (var i = 0; i < fields.length; i++) {
+    var color = fields[i].style.backgroundColor;
+    if(color == "rgb(255, 255, 255)") {
+        fields[i].style.backgroundColor = "#000";
+    }
+    else {
+        fields[i].style.backgroundColor = "#fff";
+    }
+}   
 }
